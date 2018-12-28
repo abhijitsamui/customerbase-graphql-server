46267553182f2719b50caccdc352df44798836d1
Abhijit Samui <abhijit.samui@gmail.com>
2018-12-28 16:58:54 -0700

    initial Customer schema setup in GraphQl with Hardcoded data in Array

---
+++ schema.js
@@ -0,0 +1,58 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNuull
+} = require("graphql");
+
+//Hardcoded data
+
+const customers = [
+  { id: "1", name: "John Doe", email: "john@gmail.com", age: 35 },
+  { id: "2", name: "Tanup Ghosh", email: "tanup@gmail.com", age: 33 },
+  { id: "3", name: "Abhijit Samui", email: "abhi@gmail.com", age: 32 },
+  { id: "4", name: "Pankaj Garg", email: "pankaj@gmail.com", age: 34 },
+  { id: "5", name: "Jignesh Patel", email: "jiggy@gmail.com", age: 29 }
+];
+//Customer Type
+
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parent, args) {
+        return customers;
+      }
+    }
+  }
+});
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
